f1af8ef018843b0e689d2523d56543cba190fae1
Kris Klingberg <kk0985@gmail.com>
2015-08-09 08:12:06 -0400

    Add var name, var role, and prepended both

---
+++ udacity/fewd/resume-master/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,9 @@
+var name = "Kris Klingberg"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
