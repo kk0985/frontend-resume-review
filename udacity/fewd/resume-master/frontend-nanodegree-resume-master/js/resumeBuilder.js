f7f1139a70206d83ed26622392917ca928bc3df2
Kris Klingberg <kk0985@gmail.com>
2015-08-09 11:50:04 -0400

    Var, Format and append or prepended header and contact

--- udacity/fewd/resume-master/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ udacity/fewd/resume-master/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -7,14 +7,33 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-var skills = ["MS Office", "JS", "teaching", "sales"];
+var mobile = "678-427-1056"
+var formattedmobile = HTMLmobile.replace("%data%", mobile);
 
-var bio - {
-	"name" : "Kris Klingberg",
-	"age" : 53,
-	"skills" : skills
-};
+var email = "kris.klingberg@gmail.com"
+var formattedemail = HTMLemail.replace("%data%", email);
 
-$("#main").append(bio.name);
+var github = "kk0985"
+var formattedgithub = HTMLgithub.replace("%data%", github);
+
+var twitter = "@kkling01"
+var formattedtwitter = HTMLtwitter.replace("%data%", twitter);
+
+var locale = "Atlanta"
+var formattedlocation = HTMLlocation.replace("%data%", locale);
+
+$("#topContacts").append(formattedmobile);
+$("#topContacts").append(formattedemail);
+$("#topContacts").append(formattedgithub);
+$("#topContacts").append(formattedtwitter);
+$("#topContacts").append(formattedlocation);
 
 
+var pic = "images/me.jpg"
+var formattedbioPic = HTMLbioPic.replace("%data%", pic);
+
+var message = "Hello World!"
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", message);
+
+$("#header").append(formattedbiopic);
+$("#header").append(formattedwelcomemessage);
