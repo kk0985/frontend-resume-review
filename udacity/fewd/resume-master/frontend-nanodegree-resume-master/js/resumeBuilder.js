3b995f75b084adb1748a35f33886eb098d97ac65
Kris Klingberg <kk0985@gmail.com>
2015-08-09 08:35:26 -0400

    Add skills and bio

--- udacity/fewd/resume-master/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ udacity/fewd/resume-master/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -7,3 +7,14 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+var skills = ["MS Office", "JS", "teaching", "sales"];
+
+var bio - {
+	"name" : "Kris Klingberg",
+	"age" : 53,
+	"skills" : skills
+};
+
+$("#main").append(bio.name);
+
+
